741f2f9593197fd0a81ef94f15566b365a59df8b
PRANJAY GULERIA <pranjayguleria@gmail.com>
2023-06-05 00:00:30 +0530

    Updates

---
+++ precache-manifest.835fc70188e4beeef87a635d7dc9fef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bad288483790bebde2c6",
+    "url": "/Movie_recommendation/static/css/main.f5767524.chunk.css"
+  },
+  {
+    "revision": "bad288483790bebde2c6",
+    "url": "/Movie_recommendation/static/js/main.bad28848.chunk.js"
+  },
+  {
+    "revision": "6822a5a3fe905d3996e7",
+    "url": "/Movie_recommendation/static/css/1.b09f0946.chunk.css"
+  },
+  {
+    "revision": "6822a5a3fe905d3996e7",
+    "url": "/Movie_recommendation/static/js/1.6822a5a3.chunk.js"
+  },
+  {
+    "revision": "d3bf394dd9f8ec6ba6c6",
+    "url": "/Movie_recommendation/static/js/runtime~main.d3bf394d.js"
+  },
+  {
+    "revision": "2cf20691825adfa67b497a0f8dac6d22",
+    "url": "/Movie_recommendation/index.html"
+  }
+];
